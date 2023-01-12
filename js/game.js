71683f4c9b257ea5db8bcc8e1ed04e35059dd748
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2023-01-12 13:58:42 -0500

    added html & game.js

---
+++ js/game.js
@@ -0,0 +1,28 @@
+// Created by Mikey Gloriani
+// Created on November 8 2022
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser game
+ */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  //set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // Center placement
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+const game = new Phaser.Game(config);
